9204c46a9b8b72f0c2e2ee786612223775eff5d4
SergeyBozhok <67876729+SergeyBozhok@users.noreply.github.com>
2020-09-07 18:07:31 +0300

    end

---
+++ JS/Script.js
@@ -0,0 +1,31 @@
+feedback.onclick = function(){
+   alert(' feedback! ');   
+};
+
+recearch.onclick = function(){
+   alert(' recearch!');   
+};
+
+define.onclick = function(){
+   alert(' define!');
+}
+
+ideation.onclick = function(){
+   alert(' ideation!');
+}
+
+prototype.onclick = function(){
+   alert(' prototype!');
+}
+
+user_testing.onclick = function(){
+   alert(' user_testing!');
+}
+
+design.onclick = function(){
+   alert(' design!');
+}
+
+implementation.onclick = function(){
+   alert(' implementation!');
+}
